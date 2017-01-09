73ad3720a1ea0b7c775b3e6a9098f292babd7c06
Mateusz Bartos <geekbajt@gmail.com>
2017-01-09 01:00:32 +0100

    init

---
+++ src/com/mateuszbartos/Main.java
@@ -0,0 +1,10 @@
+package com.mateuszbartos;
+
+import com.mateuszbartos.views.LoginView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LoginView mainLayout = new LoginView();
+    }
+}
